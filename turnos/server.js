471ea238fe1d4db9fc1d15ee7095c7f157701fd8
agusaguirre033 <agusaguirre033@hotmail.com>
2026-04-15 20:55:58 -0300

    Fix: remover middleware que rompia Content-Type del HTML

--- turnos/server.js
+++ turnos/server.js
@@ -14,10 +14,6 @@ const db = Datastore.create({ filename: path.join(__dirname, 'turnos.db'), autol
 
 app.use(cors());
 app.use(express.json({ limit: '1mb' }));
-app.use((req, res, next) => {
-  res.setHeader('Content-Type', 'application/json; charset=utf-8');
-  next();
-});
 app.use(express.static(path.join(__dirname, 'public')));
 
 // GET turnos ocupados de un mes (para marcar en calendario)
